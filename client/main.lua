41a91d28edcc004a14d282d5eaa8b2161813f4b5
MrMurphyOfficial <72276901+MrMurphyOfficial@users.noreply.github.com>
2022-01-25 02:34:23 +0000

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,7 +1,9 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+-- Optional, You can add or remove if you want. "admin-bypass-fakename"
+
 RegisterNetEvent('qb-template:client:fakename')
 AddEventHandler('qb-template:client:fakename', function(source, amount)
-    TriggerServerEvent("qb-template:server:callforserverside", source, amount,  -- [[Optional"admin-bypass-fakename"]] )
+    TriggerServerEvent("qb-template:server:callforserverside", source, amount, "admin-bypass-fakename" )
     TriggerEvent('QBCore:Notify', src, {text="Secret Admin v2.0", caption="You added " .. amount .. " to your bank"}, "success")
 end)
