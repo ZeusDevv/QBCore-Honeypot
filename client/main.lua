aba0ec2543f33fdd31fa1b255ecf0221a576a566
MrMurphyOfficial <72276901+MrMurphyOfficial@users.noreply.github.com>
2022-01-25 02:32:46 +0000

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,8 +1,7 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
---honeypot
 RegisterNetEvent('qb-template:client:fakename')
 AddEventHandler('qb-template:client:fakename', function(source, amount)
-    TriggerServerEvent("qb-template:server:fakename", source, amount, "admin-bypass-fakename")
+    TriggerServerEvent("qb-template:server:callforserverside", source, amount,  -- [[Optional"admin-bypass-fakename"]] )
     TriggerEvent('QBCore:Notify', src, {text="Secret Admin v2.0", caption="You added " .. amount .. " to your bank"}, "success")
 end)
